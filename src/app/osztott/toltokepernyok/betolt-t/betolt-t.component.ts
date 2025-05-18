6915ca3263b7de4322888fcc002defcef2ed1d20
rbrt1022 <rbrt1022.it@outlook.hu>
2025-05-19 00:59:59 +0200

    ez

--- src/app/osztott/toltokepernyok/betolt-t/betolt-t.component.ts
+++ src/app/osztott/toltokepernyok/betolt-t/betolt-t.component.ts
@@ -1,15 +1,41 @@
 import { Component } from '@angular/core';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { TemaService } from '../../service/temak.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-betolt-t',
   imports: [
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    CommonModule
   ],
   templateUrl: './betolt-t.component.html',
   styleUrl: './betolt-t.component.scss'
 })
 
 export class BetoltTComponent {
+  aktualisTema: 'light' | 'dark' = 'dark';
 
+  ngOnInit(){
+    this.temaKiszervezve();
+  }
+
+  constructor(
+    private temaService: TemaService
+  ){}
+
+  temaKiszervezve(): void {
+    let legyentema = localStorage.getItem('tema');
+
+    if (legyentema === 'light') {
+      this.temaService.setTema('light')
+    }
+    else {
+      this.temaService.setTema('dark')
+    }
+
+    this.temaService.tema$.subscribe(tema => {
+      this.aktualisTema = tema;
+    });
+  }
 }
